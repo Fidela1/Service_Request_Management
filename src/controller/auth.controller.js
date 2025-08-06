8830eca4d7190d08e373935d290f5d035b620360
Fidela1 <fidelatuyizere4@gmail.com>
2025-08-06 10:45:16 +0200

    added new changes

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -40,7 +40,9 @@ const createUsers = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-
+ if (!email && !password) {
+      return res.status(400).json({ error: 'Please add email and password' });
+    }
     const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'User not found' });
@@ -98,7 +100,9 @@ const createUsers = async (req, res) => {
  const deleteUserById = async (req, res) =>{
   const user_id = req.params.id;
 
+
   try{
+     
     const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [user_id]);
 
     if(result.rows.length === 0){
